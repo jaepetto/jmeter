49776eb085bc33b92cfbef84c52044d00b80b3c8
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2017-11-26 18:24:50 +0100

    fixed incorrect .gitkeep file in output preventing start of test

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 rm log.jtl
 rm jmeter.log
 rm -rf ./output/*
-./apache-jmeter-3.3/bin/jmeter -n -t load.jmx -l log.jtl -j jmeter.log -e -o output
+./apache-jmeter-3.3/bin/jmeter -n -t load.jmx -l log.jtl -j jmeter.log -e -o output -Dbasedomain=https://dcsl-next.epfl.ch -Dconcurrency=1

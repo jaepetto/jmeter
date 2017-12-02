37e0acf6eba351454c5fe596e314ad36a6d8de61
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2017-12-02 20:00:57 +0100

    Moved to constant throughput timer

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 rm log.jtl
 rm jmeter.log
 rm -rf ./output/*
+
+# Make sure that the Java interpreter is set to java 1.8. jMeter does not work with java 9 yet
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
 ./apache-jmeter-3.3/bin/jmeter -n -t load.jmx -l log.jtl -j jmeter.log -e -o output -Jduration=300
